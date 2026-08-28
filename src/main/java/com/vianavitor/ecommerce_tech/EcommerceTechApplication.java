8388792aab5217e319d5442959fa4f12a2a2e8cd
vianavitor-dev <vianavitor232@gmail.com>
2026-08-27 23:08:04 -0300

    chore: init Java Spring project environment

---
+++ src/main/java/com/vianavitor/ecommerce_tech/EcommerceTechApplication.java
@@ -0,0 +1,13 @@
+package com.vianavitor.ecommerce_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceTechApplication.class, args);
+	}
+
+}
