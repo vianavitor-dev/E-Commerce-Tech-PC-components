8827d9b77df835c1fbb0bb6387b92a33f44d6e18
vianavitor-dev <vianavitor232@gmail.com>
2026-09-07 20:07:57 -0300

    fix: lowercase the first letter of a few function names

--- src/main/java/com/vianavitor/ecommerce_tech/repositories/ProductRepository.java
+++ src/main/java/com/vianavitor/ecommerce_tech/repositories/ProductRepository.java
@@ -3,20 +3,18 @@ package com.vianavitor.ecommerce_tech.repositories;
 import com.vianavitor.ecommerce_tech.models.Product;
 import com.vianavitor.ecommerce_tech.models.aux.enums.ProductCategory;
 import com.vianavitor.ecommerce_tech.repositories.aux.ReadOnlyInterface;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-@NoRepositoryBean
 public interface ProductRepository extends ReadOnlyInterface<Product, Integer> {
     Optional<Product> findBySku(String sku);
 
-    List<Product> FindByNameContaining(String name);
+    List<Product> findByNameContaining(String name);
 
-    List<Product> FindByCategory(ProductCategory category);
+    List<Product> findByCategory(ProductCategory category);
 
-    List<Product> FindByCategoryAndNameContaining(ProductCategory category, String name);
+    List<Product> findByCategoryAndNameContaining(ProductCategory category, String name);
 }
