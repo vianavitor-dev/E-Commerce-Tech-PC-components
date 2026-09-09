98d2254665f0faac4e9cb8d90b463b082599f638
vianavitor-dev <vianavitor232@gmail.com>
2026-09-09 18:41:26 -0300

    feat: update searching products name methods to implement full-text index

--- src/main/java/com/vianavitor/ecommerce_tech/repositories/ProductRepository.java
+++ src/main/java/com/vianavitor/ecommerce_tech/repositories/ProductRepository.java
@@ -3,6 +3,7 @@ package com.vianavitor.ecommerce_tech.repositories;
 import com.vianavitor.ecommerce_tech.models.Product;
 import com.vianavitor.ecommerce_tech.models.aux.enums.ProductCategory;
 import com.vianavitor.ecommerce_tech.repositories.aux.ReadOnlyInterface;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,9 +13,19 @@ import java.util.Optional;
 public interface ProductRepository extends ReadOnlyInterface<Product, Integer> {
     Optional<Product> findBySku(String sku);
 
+    @Query(value = """
+            SELECT * 
+            FROM products WHERE MATCH(name) AGAINST(:name)
+            """,
+            nativeQuery = true)
     List<Product> findByNameContaining(String name);
 
     List<Product> findByCategory(ProductCategory category);
 
+    @Query(value = """
+            SELECT * 
+            FROM products WHERE category = :category AND MATCH(name) AGAINST(:name)
+            """,
+            nativeQuery = true)
     List<Product> findByCategoryAndNameContaining(ProductCategory category, String name);
 }
