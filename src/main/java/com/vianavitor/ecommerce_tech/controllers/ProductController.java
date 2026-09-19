9f337efcd7cf1e4cd7a39262d92cf87e134c0472
vianavitor-dev <vianavitor232@gmail.com>
2026-09-18 23:56:18 -0300

    feat(controller): add rate endpoint to products

--- src/main/java/com/vianavitor/ecommerce_tech/controllers/ProductController.java
+++ src/main/java/com/vianavitor/ecommerce_tech/controllers/ProductController.java
@@ -1,16 +1,18 @@
 package com.vianavitor.ecommerce_tech.controllers;
 
 import com.vianavitor.ecommerce_tech.dtos.request.PcComponentsDTO;
+import com.vianavitor.ecommerce_tech.dtos.request.ProductUserIdsDTO;
 import com.vianavitor.ecommerce_tech.dtos.response.PcCompatibilityCheckResultDTO;
 import com.vianavitor.ecommerce_tech.dtos.response.ProductSearchResultsDTO;
 import com.vianavitor.ecommerce_tech.models.Product;
+import com.vianavitor.ecommerce_tech.models.aux.enums.ProductCategory;
 import com.vianavitor.ecommerce_tech.services.ProductService;
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -47,5 +49,14 @@ public class ProductController {
         List<? extends ProductSearchResultsDTO> results = service.findAll();
 
         return ResponseEntity.ok(results);
+    }
+    
+    @PostMapping("/rate")
+    public BigDecimal rate(@RequestBody ProductUserIdsDTO request, @RequestParam BigDecimal value) {
+        if (value == null) {
+            return service.removesRate(request.productId(), request.userId());
+        }
+
+        return service.rateProduct(request.productId(), request.userId(), value);
     }
 }
---
+++ src/main/java/com/vianavitor/ecommerce_tech/dtos/request/ProductUserIdsDTO.java
@@ -0,0 +1,6 @@
+package com.vianavitor.ecommerce_tech.dtos.request;
+
+public record ProductUserIdsDTO(
+        Integer productId, Integer userId
+) {
+}
